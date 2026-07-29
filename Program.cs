5a025d0740c84a13f0c181333b855c276166b75e
arnili64 <arinil38400@gmail.com>
2026-07-29 12:30:38 +0330

    Update Program.cs all viewer's comments solved
    names fixed
    metods cases 
    dictionary srearch fixed
    define stats

--- Program.cs
+++ Program.cs
@@ -6,75 +6,37 @@ namespace BrowserHistory
 {
     class Program
     {
+        private const int DefaultTopStatsCount = 3;
+
         static void Main(string[] args)
         {
             Stack<string> BackSearch = new Stack<string>();
-            
             Stack<string> ForwardSearch = new Stack<string>();
-            
             Dictionary<string, int> searchStats = new Dictionary<string, int>();
 
             while (true)
             {
                 Console.Write("> ");
-                string input = Console.ReadLine()?.Trim();
-                
+                string input = Console.ReadLine()?.Trim() ?? "";
+
                 if (string.IsNullOrEmpty(input)) continue;
 
-                string[] commandArgs = input.Split(' ', 2);
+                string[] commandArgs = input.Split(' ', 2, StringSplitOptions.RemoveEmptyEntries);
                 string command = commandArgs[0].ToUpper();
+                string argument = commandArgs.Length > 1 ? commandArgs[1].Trim() : string.Empty;
 
                 switch (command)
                 {
                     case "SEARCH":
-                        if (commandArgs.Length < 2)
-                        {
-                            Console.WriteLine("Please provide a search term.");
-                            break;
-                        }
-                        string term = commandArgs[1];
-                        
-                        BackSearch.Push(term);
-                        ForwardSearch.Clear();
-
-                        if (searchStats.ContainsKey(term))
-                        {
-                            searchStats[term]++;
-                        }
-                        else
-                        {
-                            searchStats[term] = 1;
-                        }
-                        
-                        PrintCurrent(BackSearch);
+                        HandleSearch(argument, BackSearch, ForwardSearch, searchStats);
                         break;
 
                     case "BACK":
-                        if (BackSearch.Count > 1) 
-                        {
-                            ForwardSearch.Push(BackSearch.Pop());
-                            PrintCurrent(BackSearch);
-                        }
-                        else if(BackSearch.Count == 1)
-                        {
-                            Console.WriteLine("You can't use back!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("History is empty.");
-                        }
+                        HandleBack(BackSearch, ForwardSearch);
                         break;
 
                     case "FORWARD":
-                        if (ForwardSearch.Count > 0)
-                        {
-                            BackSearch.Push(ForwardSearch.Pop());
-                            PrintCurrent(BackSearch);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Cannot go forward.");
-                        }
+                        HandleForward(BackSearch, ForwardSearch);
                         break;
 
                     case "CURRENT":
@@ -82,18 +44,11 @@ namespace BrowserHistory
                         break;
 
                     case "STATS":
-                        var topSearches = searchStats
-                            .OrderByDescending(x => x.Value)
-                            .Take(3);
-                            
-                        foreach (var stat in topSearches)
-                        {
-                            Console.WriteLine($"{stat.Key}: {stat.Value}");
-                        }
+                        HandleStats(searchStats, DefaultTopStatsCount);
                         break;
 
                     case "UNIQUE":
-                        Console.WriteLine(searchStats.Count);
+                        HandleUnique(searchStats);
                         break;
 
                     case "EXIT":
@@ -106,11 +61,80 @@ namespace BrowserHistory
             }
         }
 
+        static void HandleSearch(string term, Stack<string> BackSearch, Stack<string> ForwardSearch, Dictionary<string, int> searchStats)
+        {
+            if (string.IsNullOrWhiteSpace(term))
+            {
+                Console.WriteLine("Please provide a search term.");
+                return;
+            }
+
+            BackSearch.Push(term);
+            ForwardSearch.Clear();
+
+            searchStats[term] = searchStats.GetValueOrDefault(term, 0) + 1;
+
+            PrintCurrent(BackSearch);
+        }
+
+        static void HandleBack(Stack<string> BackSearch, Stack<string> ForwardSearch)
+        {
+            if (BackSearch.Count > 1)
+            {
+                ForwardSearch.Push(BackSearch.Pop());
+                PrintCurrent(BackSearch);
+            }
+            else if (BackSearch.Count == 1)
+            {
+                Console.WriteLine("You can't use back!");
+            }
+            else
+            {
+                Console.WriteLine("History is empty.");
+            }
+        }
+
+        static void HandleForward(Stack<string> BackSearch, Stack<string> ForwardSearch)
+        {
+            if (ForwardSearch.Count > 0)
+            {
+                BackSearch.Push(ForwardSearch.Pop());
+                PrintCurrent(BackSearch);
+            }
+            else
+            {
+                Console.WriteLine("Cannot go forward.");
+            }
+        }
+
+        static void HandleStats(Dictionary<string, int> searchStats, int topCount = DefaultTopStatsCount)
+        {
+            if (searchStats.Count == 0)
+            {
+                Console.WriteLine("No search history available.");
+                return;
+            }
+
+            var topSearches = searchStats
+                .OrderByDescending(x => x.Value)
+                .Take(topCount);
+
+            foreach (var stat in topSearches)
+            {
+                Console.WriteLine($"{stat.Key}: {stat.Value}");
+            }
+        }
+
+        static void HandleUnique(Dictionary<string, int> searchStats)
+        {
+            Console.WriteLine(searchStats.Count);
+        }
+
         static void PrintCurrent(Stack<string> stack)
         {
             if (stack.Count > 0)
             {
-                Console.WriteLine($"current: {stack.Peek()}"); 
+                Console.WriteLine($"current: {stack.Peek()}");
             }
             else
             {
