20261584edb6ca9920ac9c45d18ae12e78852db7
Negar-Erfani <negarer1384@gmail.com>
2026-08-15 15:24:24 +0330

    initial code

---
+++ ASP.Net_core/Controllers/SimpleController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Net_core.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[Action]")]
+    public class SimpleController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello world!";
+        }
+    }
+}
